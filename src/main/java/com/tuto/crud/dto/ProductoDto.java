225ce1c6b93b037e0d7512721021c0f026f119ac
andres74ar@gmail.com <andres74ar@gmail.com>
2022-09-01 09:57:26 -0300

    fin video 3

--- src/main/java/com/tuto/crud/dto/ProductoDto.java
+++ src/main/java/com/tuto/crud/dto/ProductoDto.java
@@ -16,5 +16,21 @@ public class ProductoDto {
         this.nombre = nombre;
         this.precio = precio;
     }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public Float getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Float precio) {
+        this.precio = precio;
+    }
     
 }
